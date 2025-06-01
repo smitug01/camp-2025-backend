3c5fb20cccc87ffb19c60a10ea5dad99bb083c9c
Ricky Lu <sumily20@gmail.com>
2025-06-01 20:36:46 +0800

    Refactor code structure for improved readability and maintainability

---
+++ app.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Item(BaseModel):
+    item_id: int
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World, This a sample FastAPI endpoint."}
+
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse('favicon.ico')
+
+
+@app.get("/item/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/items/")
+async def list_items():
+    return [{"item_id": 1, "name": "Foo"}, {"item_id": 2, "name": "Bar"}]
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
